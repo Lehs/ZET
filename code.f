4fb02764e633e6d4d46ed1b9877c7bd7d738be47
Lars-Erik Svahn <lehs@hotmail.se>
2016-02-12 10:24:10 +0100

    Update code.f

--- code.f
+++ code.f
@@ -511,8 +511,10 @@ cell 4 =
   while 2 -
   repeat ;
 
-?undef under+ [if]
-: under+ ( a b c -- a+c b ) rot + swap ; [then]
+?undef under+ 
+[if]
+: under+ ( a b c -- a+c b ) rot + swap ; 
+[then]
 
 : 8/mod ( n -- r q ) dup 7 and swap 3 rshift ;
 : 256/mod ( n -- r q ) dup 255 and swap 8 rshift ;
@@ -697,16 +699,11 @@ cell negate constant -cell
 : >>yst ( xn ... x1 bc -- )  >r r@ cs 0 ?do >yst loop r> >yst ;
 : yst>> ( -- x1 ... xn bc )  yst@ >r yst> cs 0 ?do yst> loop r> ; 
 
-: ydrop  yst @ @ cs cells cell+ negate yst +! ;
-
 cell 1- log~ constant cellshift
 
-: @depth ( ad -- n )  dup @ swap - cellshift rshift ;
-: @cl ( ad -- )  dup ! ;
-: @empty ( ad -- flag )  dup @ = ;
-\ example: xst @empty
-\ _depth _cl _empty _fence
-\ @depth @cl @empty @fence
+: stack-depth ( ad -- n )  dup @ swap - cellshift rshift ;
+: stack-cl ( ad -- )  dup ! ;
+: stack-empty ( ad -- flag )  dup @ = ;
 
 1 21 lshift cells allocate throw dup constant zst dup ! 
 
@@ -720,8 +717,10 @@ cell 1- log~ constant cellshift
 : >>zst ( xn ... x1 bc -- )  >r r@ cs 0 ?do >zst loop r> >zst ;
 : zst>> ( -- x1 ... xn -n )  zst@ >r zst> cs 0 ?do zst> loop r> ;
 
-: showz zst @depth if zst> >r recurse r> dup . >zst then ;
-: showy yst @depth if yst> >r recurse r> dup . >yst then ;
+: showx xst stack-depth if xst> >r recurse r> dup . >xst then ;
+: showy yst stack-depth if yst> >r recurse r> dup . >yst then ;
+: showx zst stack-depth if zst> >r recurse r> dup . >zst then ;
+
 : >zet ( s -- | -- s)  >>yst yst> dup >r cs 0 ?do yst> >zst loop r> >zst ;
 : zet> ( -- s | s -- )  zst> dup >r cs 0 ?do zst> >xst loop r> >xst xst>> ;
 
@@ -784,7 +783,7 @@ cell 1- log~ constant cellshift
 : a>addr1 \ c -- 
   addr1 c! addr1- ;
 
-: card \ -- n | s --
+: cardinality \ -- n | s --
   zst> cs dup >xst 0
   ?do zst@ 0<
      if zst@ dup cs negate xst+! >r zdrop r> cs 1+
@@ -793,7 +792,7 @@ cell 1- log~ constant cellshift
   +loop xst> ;
 
 : foreach \ -- n 0 | s -- z1...zn
-  zdup card zst> drop 0 ;
+  zdup cardinality zst> drop 0 ;
 
 : closep \ -- bc asc
   zst@ dup listflag if [char] ) else [char] } then ;
@@ -820,7 +819,7 @@ cell 1- log~ constant cellshift
   else printbuf list$ type
   then ; 
 
-: @check. \ yst,xst -- 
+: set. \ yst,xst -- 
   zst setcopy zet. ;
 
 \ Analyzing sets_____
@@ -832,13 +831,19 @@ cell 1- log~ constant cellshift
   else drop 0
   then ;
 
-: @split \ ad --   ad=yst,zst 
+: _split \ ad --   ad=yst,zst 
   dup >r @ cell- @ 0< 0=
   if r@ stack> 2 + r@ stack> swap r@ >stack r> >stack exit then
   r@ stack>
   r@ xst setmove
   xst@ cs 1+ 2* + r@ >stack
   xst r> setmove ;
+
+: ysplit \ -- | s -- s' x  in yst stack
+  yst _split ;
+
+: zsplit \ -- | s -- s' x
+  zst _split ;
 
 \ Set equal, subset and membership_____
 
@@ -893,17 +898,17 @@ cell 1- log~ constant cellshift
 : subset \ -- flag | s s' --
   'subset execute ;
 
-: set= \ -- flag | s s' --
+: zet= \ -- flag | s s' --
   zover zover subset
   if zswap subset
   else zdrop zdrop false
   then ; 
 
-: set-member \ -- flag | s s' -- 
+: zet-member \ -- flag | s s' -- 
   zswap zst yst setmove
   begin zst@                         \ set not empty?
-  while zst @split zst@ ?obj 2 =      \ element is a set?
-     if yst zst setcopy set=  
+  while zsplit zst@ ?obj 2 =      \ element is a set?
+     if yst zst setcopy zet=  
         if yst setdrop zdrop true exit then
      else zst@ ?obj if zdrop else zst> drop then
      then 
@@ -913,7 +918,7 @@ cell 1- log~ constant cellshift
   secobjad @ ?obj
   case 0 of routout smember endof
        1 of vmember endof
-       2 of set-member endof
+       2 of zet-member endof
   endcase ;
 
 :noname \ -- flag | s s' --          \ the subset code
@@ -921,7 +926,7 @@ cell 1- log~ constant cellshift
   if zdrop zdrop true exit then      \ true if both sets are empty
   zswap zst yst setmove
   begin yst@                         \ set is not empty?
-  while yst @split yst@ ?obj
+  while ysplit yst@ ?obj
      if yst zst setmove zover member
      else yst> zdup smember 
      then 0= if yst setdrop zdrop false exit then
@@ -929,7 +934,7 @@ cell 1- log~ constant cellshift
 
 \ Set algebra_____
 
-: merge \ -- | s s' -- s"
+: zetmerge \ -- | s s' -- s"
   zst yst setmove
   yst@ zst> + 
   yst zst setmove
@@ -941,32 +946,36 @@ cell 1- log~ constant cellshift
   yst zst setmove
   zst! ;
 
-: @fence \ ad -- | x -- {x} 
+: _fence \ ad -- | x -- {x} 
   dup >r stack@ ?obj 
   case 0 of -2 r@ >stack endof 
        1 of r@ stack@ 1- r@ >stack endof
        2 of r@ stack@ 2 - r@ >stack endof
   endcase rdrop ;
 
-: reduce \ -- | s -- s'   ??? recursivt!!!
+: xfence xst _fence ;
+: yfence yst _fence ;
+: zfence zst _fence ;
+
+: reduce \ -- | s -- s' 
   0 >yst foreach
-  ?do zst @fence zdup zst> drop
+  ?do zfence zdup zst> drop
      yst zst setcopy member
      if zdrop
      else yst zst setmove
-        merge zst yst setmove
+        zetmerge zst yst setmove
      then
   loop yst zst setmove ;
 
 : union \ -- | s s' -- s"
-  merge reduce ;
+  zetmerge reduce ;
 
 : intersection \ -- | s s' -- s" 
   0 >xst zst yst setmove
   begin zst@
-  while zst @split zst @fence zdup zst> drop
+  while zsplit zfence zdup zst> drop
      yst zst setcopy member
-     if xst zst setmove merge zst xst setmove
+     if xst zst setmove zetmerge zst xst setmove
      else zdrop
      then 
   repeat zdrop yst setdrop
@@ -975,91 +984,105 @@ cell 1- log~ constant cellshift
 : diff \ s s' -- s" 
   0 >xst zst yst setmove 
   begin zst@
-  while zst @split zst @fence zdup zst> drop
+  while zsplit zfence zdup zst> drop
      yst zst setcopy member
      if zdrop 
-     else xst zst setmove merge zst xst setmove
+     else xst zst setmove zetmerge zst xst setmove
      then
   repeat zdrop yst setdrop
   xst zst setmove reduce ;
 
-\ s is a set of sets 
-\ in each element in s x is included 
-: multincl \ -- | s x -- s'    ???
-  0 >xst zst @fence zst yst setmove 
-  begin zst@ 
-  while zst @split yst zst setcopy union zst @fence 
-     xst zst setmove union zst xst setmove 
-  repeat zdrop yst setdrop 
-  xst zst setmove ; 
+: multincl \ s x -- s'
+  0 >xst zfence zst yst setmove
+  begin zst@
+  while zsplit yst zst setcopy union zfence
+     xst zst setmove union zst xst setmove
+  repeat zdrop yst setdrop xst zst setmove ;
 
-: powerset \ s -- s' 
-  zst@ 0= if -2 >zst exit then 
-  zst @split zst @fence zst yst setmove recurse 
-  zdup yst zst setmove zst> drop multincl union ; 
+: powerset \ s -- s'
+  zst@ 0= if -2 >zst exit then
+  zsplit zfence zst yst setmove recurse
+  zdup yst zst setmove zst> drop multincl union ;
 
 : cartprod \ s s' -- s"
   zst yst setmove
-  zst xst setcopy xst> drop card 0 0 >zst
-  ?do xst @fence -1 xst+! 
+  zst xst setcopy xst> drop cardinality 0 0 >zst
+  ?do xfence -1 xst+! 
      yst setdup
      begin yst@
-     while yst @split yst @fence -1 yst+!
+     while ysplit yfence -1 yst+!
         xst zst setcopy
         yst zst setmove vmerge
-        zst @fence union
+        zfence union
      repeat yst> drop xst setdrop 
   loop yst setdrop ;
 
 \ {x1,...,xn} -- {{x1},...,{xn}}
 : infence \ -- | s -- s' 
   0 >xst foreach 
-  ?do zst @fence zst @fence
+  ?do zfence zfence
      xst zst setmove union
      zst xst setmove 
   loop xst zst setmove ; 
 
 \ p(A,k)=p(A\{f(A)},k)+(p(A\{f(A)},k-1)%f(A))
 : power# \ n -- | s -- s' 
-  dup zdup card = if drop zst @fence exit then 
+  dup zdup cardinality =
+  if drop zfence exit then 
   dup 1 = if drop infence exit then 
-  zst @split zst @fence zst xst setmove 
+  zsplit zfence zst xst setmove 
   dup zdup recurse 
   zswap 1- recurse xst zst setmove
   zst> drop multincl union ; 
 
+\ {s1,...,sn} -- s1U...Usn
 : multiunion \ -- | s -- s'
   foreach 0 >zst
   ?do union loop ;
 
 \ {s1,...,sn} s' -- {s1Us',...,snUs'}
-: setcup \ -- | s s' -- s"
+: zetcup \ -- | s s' -- s"
   zst xst setmove 0 >yst foreach
-  ?do xst zst setcopy union zst @fence
+  ?do xst zst setcopy union zfence
      yst zst setmove union zst yst setmove
   loop xst setdrop yst zst setmove ;
 
 \ {s1,...,sn} s' -- {s1&s',...,sn&s'}
-: setcap \ -- | s s' -- s" 
-  zst xst setmove 0 >yst foreach 
-  ?do xst zst setcopy intersection zst @fence 
-  yst zst setmove union zst yst setmove 
-  loop xst setdrop yst zst setmove ; 
+: zetcap \ -- | s s' -- s"
+  zst xst setmove 0 >yst foreach
+  ?do xst zst setcopy intersection zfence
+     yst zst setmove union zst yst setmove
+  loop xst setdrop yst zst setmove ;
 
-\ Input of sets_____ 
+\ { s1,...,sn} {t1,...,tm} -- {siUtj}ij
+: zetunion \ -- | s s' -- s"
+  0 >xst zst yst setmove foreach
+  ?do yst zst setcopy
+     zswap zetcup
+     xst zst setmove union
+     zst xst setmove
+  loop yst setdrop xst zst setmove ; 
 
-0 create match , 
+: func \ -- | s s' -- s"
+  secobjad @ 0= if zdrop -2 exit then
+  secobjad @ -2 = if cartprod infence exit then
+  zswap zsplit zfence zst xst setmove
+  zover recurse zswap xst zst setmove zswap cartprod infence zetunion ;
 
-: { \ -- 
-  1 match +! depth >xst ; 
+\ Input of sets_____
+
+0 create match ,
+
+: { \ --
+  1 match +! depth >xst ;
 
 : } \ x1...xk -- 
-  depth xst> - 2* negate 
+  depth xst> - 2* negate
   -1 match +! >zet reduce match @ if zet> then ; 
 
-: q  xst @cl yst @cl zst @cl 0 match ! abort ; 
+: q  xst stack-cl yst stack-cl zst stack-cl 0 match ! abort ;
 
-: (  { ; 
+: (  { ;
 
 : ) \ x1...xk --
   depth xst> - 2* 1+ negate
@@ -1069,9 +1092,15 @@ cell 1- log~ constant cellshift
 : odd 1 and ;  \ n -- flag
 : 1mod4 4 mod 1 = ;  \ n -- flag
 : 3mod4 4 mod 3 = ;  \ n -- flag
+: sqr dup sqrtf dup * = ;
+
+: 2sqrsum dup 0
+  ?do dup i dup * - dup
+     0< if drop false leave then 
+     sqr if true leave then
+  loop nip ;
 
 \ 30 70 | odd
 : | \ m n -- x1...xk 
   swap ' loc{ xt }
   ?do i xt execute if i then loop ;
-
